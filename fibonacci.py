c2df7c6da7a363db851da122baa2c1e5e31c7d9d
Tomas Gutierrez <tgut03@gmail.com>
2025-10-09 16:20:34 -0500

    move input from main to generate_fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,9 @@
 import turtle as t
 
-def generate_fibonacci(n):
+def generate_fibonacci():
+    n = input("n = "); n = int(n)
     fib = [0, 1]
+
     for i in range(n+1):
         sum = fib[i+1] + fib[i]
         fib.append(sum)
@@ -12,8 +14,7 @@ def plot_fibonacci(fib):
         t.circle(num, 90)
 
 def main():
-    n0 = input("n = "); n0 = int(n0)
-    fib0 = generate_fibonacci(n0)
+    fib0 = generate_fibonacci()
     plot_fibonacci(fib0)
 
 main()
